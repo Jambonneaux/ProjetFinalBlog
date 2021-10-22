bbe56a9842d7392aea49298a7660023683f44383
Jonathan Bonenfant <bonenfantjo97@gmail.com>
2021-10-22 19:36:39 -0400

    Changement de la connexion vers localhost

--- lib/connex.php
+++ lib/connex.php
@@ -2,6 +2,9 @@
 
 //Connexion vers ma base de donné
 
-  $con = mysqli_connect("localhost", "e2095741", "NArhymrB87R2BKRfL9m4", "e2095741");
+  //$con = mysqli_connect("localhost", "e2095741", "NArhymrB87R2BKRfL9m4", "e2095741");
+  //mysqli_set_charset($con,"utf8");
+
+  $con = mysqli_connect("localhost", "root", "", "forumDB");
   mysqli_set_charset($con,"utf8");
  ?>
